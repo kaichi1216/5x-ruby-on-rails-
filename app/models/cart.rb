06d484d30d63cc793f9784cedd8db966401fe7b2
kai <jok002787@gmail.com>
2019-09-06 12:59:52 +0800

    add cartitem

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 
 class Cart
+attr_reader :items
 
     def initialize
       @items = []
@@ -11,7 +12,14 @@ class Cart
     end
   
     def add_item(product_id)
-      @items << product_id
+    found_item = @items.find {|i| i.product_id == product_id}
+      if found_item
+        found_item.increment(1)
+      else
+        @items << CartItem.new(product_id)
+      end
     end
+
   
 end
+
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "可以把商品丟到購物車裡 然後購物車就會有東西"do 
-    cart = Cart.new
-    expect(cart.empty?).to be true
+  context '基本功能' do
+    it "可以把商品丟到購物車裡 然後購物車就會有東西"do 
+      cart = Cart.new
+        expect(cart.empty?).to be true
 
-    cart.add_item(1)
-    expect(cart.empty?).to be false
+      cart.add_item(1)
+        expect(cart.empty?).to be false
+    end
+
+    it '加同種類的商品 購買項目 不會增加 但商品數量會改變' do
+      cart = Cart.new
+      3.times {cart.add_item(1)}
+      2.times {cart.add_item(2)}
+      2.times {cart.add_item(1)}
+
+      expect(cart.items.count).to be 2
+    end
   end
+
+
 end
